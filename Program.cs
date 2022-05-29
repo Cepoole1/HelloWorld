af62b7d7431234ccdc9f63185d6dc00f08af7c00
chandler <cepoole1@crimson.ua.edu>
2022-05-28 20:10:23 -0500

    INTRO

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// Our first project
+// Processing starts at the top and goes to the bottom
+
+// Console.WriteLine("Welcome to my application!");
+// Console.WriteLine("My name is Chandler Poole");
+
+// Console.WriteLine("5");
+// Console.WriteLine(5+5);
+// Console.WriteLine("5"+"5");
+// Console.WriteLine("Chandler"+"Poole");
+
+// strings
+string firstName; //camel casing 
+
+
+//int
+int age;
+
+// double
+double myDouble;
+
+//bool
+bool isStudent;
+
+
